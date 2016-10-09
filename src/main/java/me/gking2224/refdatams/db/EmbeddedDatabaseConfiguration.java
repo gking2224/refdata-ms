d4a4a06187ae4602e63e62d28df1ef6cdc430aa3
Graham King <gking2224@gmail.com>
2016-10-09 21:14:21 +0100

    Minor

--- src/main/java/me/gking2224/refdatams/db/EmbeddedDatabaseConfiguration.java
+++ src/main/java/me/gking2224/refdatams/db/EmbeddedDatabaseConfiguration.java
@@ -2,7 +2,6 @@ package me.gking2224.refdatams.db;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
