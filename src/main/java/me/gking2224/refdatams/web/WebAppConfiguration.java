3c2d3c09e8629d2b67e44a54da41756e1426d54d
Graham King <gking2224@gmail.com>
2016-10-17 14:59:35 +0100

    keeping up with dependency changes

--- src/main/java/me/gking2224/refdatams/web/WebAppConfiguration.java
+++ src/main/java/me/gking2224/refdatams/web/WebAppConfiguration.java
@@ -1,34 +1,15 @@
 package me.gking2224.refdatams.web;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import me.gking2224.common.web.CommonWebAppConfiguration;
-import me.gking2224.common.web.WebConfigurationOptions;
 
 @Profile("web")
 @ComponentScan({"me.gking2224.refdatams.web"})
 @Import(CommonWebAppConfiguration.class)
 @EnableWebMvc
 public class WebAppConfiguration {
-    
-    @Bean
-    public WebConfigurationOptions getConfig() {
-        return new WebConfigurationOptions() {
-
-            @Override
-            public String[] getAllowedCorsOrigins() {
-                return new String[] {"http://localhost:8080"};
-            }
-
-            @Override
-            public String[] getAllowedCorsMethods() {
-                return null;
-            }
-            
-        };
-    }
 }
