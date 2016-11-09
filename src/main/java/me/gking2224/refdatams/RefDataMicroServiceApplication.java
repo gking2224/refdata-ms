4a90c49ac27e0a474b376f827260f9d5626b0a91
Graham King <gking2224@gmail.com>
2016-11-09 11:43:45 +0000

    Revert "create mbean server"
    This reverts commit 2a7a863938afb387f25ca49758b5970e737416d7.

--- src/main/java/me/gking2224/refdatams/RefDataMicroServiceApplication.java
+++ src/main/java/me/gking2224/refdatams/RefDataMicroServiceApplication.java
@@ -24,6 +24,7 @@ import me.gking2224.refdatams.web.WebAppConfiguration;
     BatchConfiguration.class,
     WebAppConfiguration.class,
     AopConfiguration.class,
+    JmxConfiguration.class
 })
 public class RefDataMicroServiceApplication extends AbstractMicroServiceApplication {
 
